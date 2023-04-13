423c11120099669562c393ddbc75413f383a747e
jarojasres <jarojasres@gmail.com>
2023-04-12 21:44:42 -0500

    Add google maps excercise

---
+++ maps/src/index.ts
@@ -0,0 +1,14 @@
+import { User } from './User';
+import {Company } from './Company';
+import { CustomMap } from './CustomMap';
+
+const user = new User();
+const company = new Company();
+console.log(user);
+console.log(company);
+
+
+const map = new CustomMap('map');
+
+map.addMarker(user);
+map.addMarker(company);
