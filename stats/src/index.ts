877cafec5d3098953b4cd920db320a99021bb9fe
jarojasres <jarojasres@gmail.com>
2023-04-16 15:31:59 -0500

    Refactor composition

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -10,23 +10,23 @@
 import { MatchReader } from './MatchReader';
 import { CsvFileReader } from './CsvFileReader';
 import { MatchResult } from './MatchResult';
+import { Summary } from './Summary';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { HtmlReport } from './reportTargets/HtmlReport';
 
 // Create an object that satisfies the 'DataReader' interface
 const csvFileReader = new CsvFileReader('football.csv');
 
 // Create an instance of MatchReader and pass in something satisfying
 // the 'DataReader' interface
-const matchReader = new MatchReader(csvFileReader);
+// const matchReader = new MatchReader(csvFileReader);
+// matchReader.load();
+
+const matchReader = MatchReader.fromCsv('football.csv');
+const summary = Summary.winsAnalysisWithHtmlReport("Man United");
+
 matchReader.load();
+summary.buildAndPrintReport(matchReader.matches);
 
-let manUnitedWins = 0;
-
-for (let match of matchReader.matches) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(`Man United won ${manUnitedWins} games`);
+//console.log(`Man United won ${0} games`);
