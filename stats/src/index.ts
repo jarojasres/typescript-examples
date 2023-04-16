8efe8af3c09438aad95c1d2aa18538abb7208d0a
jarojasres <jarojasres@gmail.com>
2023-04-16 14:30:54 -0500

    Add inheritance example

---
+++ stats/src/index.ts
@@ -0,0 +1,32 @@
+// Inheritance
+// import { MatchReader } from './inheritance/MatchReader';
+
+// const reader = new MatchReader('football.csv');
+// reader.read();
+
+// console.log(reader.data[0]);
+
+
+import { MatchReader } from './MatchReader';
+import { CsvFileReader } from './CsvFileReader';
+import { MatchResult } from './MatchResult';
+
+// Create an object that satisfies the 'DataReader' interface
+const csvFileReader = new CsvFileReader('football.csv');
+
+// Create an instance of MatchReader and pass in something satisfying
+// the 'DataReader' interface
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
+
+let manUnitedWins = 0;
+
+for (let match of matchReader.matches) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
